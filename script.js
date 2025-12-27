d92a3100a8ab280cec3cabdef3b60c7a5c48bd81
Rafa <75946637+rafabruno263@users.noreply.github.com>
2025-12-27 13:20:48 -0300

    Atualizado Script

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+let recordePontos = 0;
+let recordeTecla = "---";
+
+function modo4Teclas() {
+    // Esconde as teclas 5, 6, 7 e 8
+    document.getElementById("box-5").style.display = "none";
+    document.getElementById("box-6").style.display = "none";
+    document.getElementById("box-7").style.display = "none";
+    document.getElementById("box-8").style.display = "none";
+}
+
+function modo8Teclas() {
+    // Mostra todas
+    document.getElementById("box-5").style.display = "flex";
+    document.getElementById("box-6").style.display = "flex";
+    document.getElementById("box-7").style.display = "flex";
+    document.getElementById("box-8").style.display = "flex";
+}
+
 let contadores = {
     '1': 0,
     '2': 0,
     '3': 0,
-    '4': 0
+    '4': 0,
+    '5': 0,
+    '6': 0,
+    '7': 0,
+    '8': 0
 };
 
 let jogoAtivo = false;
@@ -22,11 +45,15 @@ document.addEventListener("keydown", (event) => {
 });
 
 function iniciarJogo() {
-    contadores = {'1': 0, '2': 0, '3': 0, '4': 0};
+    contadores = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0};
     document.getElementById("count1").innerText = 0;
     document.getElementById("count2").innerText = 0;
     document.getElementById("count3").innerText = 0;
     document.getElementById("count4").innerText = 0;
+    document.getElementById("count5").innerText = 0;
+    document.getElementById("count6").innerText = 0;
+    document.getElementById("count7").innerText = 0;
+    document.getElementById("count8").innerText = 0;
     displayRes.innerText = "";
 
     jogoAtivo = true;
@@ -48,18 +75,36 @@ function iniciarJogo() {
 function finalizarJogo() {
     jogoAtivo = false;
     btnStart.disabled = false;
-    displayTimer.innerText = "Tempo Esgotado!";
+    btnStart.innerText = "JOGAR NOVAMENTE";
+    displayTimer.innerText = "TIME: 00:00";
 
     let entradas = Object.entries(contadores);
+    const placar2Visivel = document.getElementById("box-5").style.display !== "none";
+    
+    if (!placar2Visivel) {
+        entradas = entradas.filter(entry => parseInt(entry[0]) <= 4);
+    }
 
     entradas.sort((a, b) => b[1] - a[1]);
 
     let vencedor = entradas[0];
 
     if (vencedor[1] === 0) {
-        displayRes.innerText = "Você não apertou nada!";
+        displayRes.innerText = "NENHUM CLIQUE FOI DETECTACO";
+        displayRes.style.color = "#888";
     } else {
-        displayRes.innerText = `A tecla mais clicada foi "${vencedor[0]}" com ${vencedor[1]} cliques!`;
-        displayRes.style.color = "green";
+        displayRes.innerText = `VENCEDOR: JOGADOR ${vencedor[0]}  (${vencedor[1]} CLIQUES)`;
+        displayRes.style.color = "#fff";
+    }
+
+    if (vencedor[1] > recordePontos) {
+        recordePontos = vencedor[1];
+        recordeTecla = vencedor[0];
+        
+        const highScoreEl = document.getElementById("high-score");
+        highScoreEl.innerText = `HIGH SCORE: JOGADOR ${recordeTecla} - ${recordePontos}`;
+        
+        highScoreEl.style.color = "#4dff88"; 
+        setTimeout(() => { highScoreEl.style.color = "#888"; }, 2000);
     }
 }
